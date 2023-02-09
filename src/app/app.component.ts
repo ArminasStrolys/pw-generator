7ccae5329971657b1e147dc0bd0fa30803625a25
Arminas Strolys <arminas.strolys@gmail.com>
2023-02-09 21:26:38 +0200

    Got a functioning password generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,37 @@ export class AppComponent {
   useLetters = false;
   useNumbers = false;
   useSymbols = false;
+  passwordLength = 0;
+
   // title = 'pw-generator';
   onButtonClick() {
-    console.log('l', this.useLetters);
-    console.log('n', this.useNumbers);
-    console.log('s', this.useSymbols);
-    this.password = 'PW';
+    // console.log('l', this.useLetters);
+    // console.log('n', this.useNumbers);
+    // console.log('s', this.useSymbols);
+    // console.log('PW l', this.passwordLength);
+    // this.password = 'PW';
+    const numbers = '0123456789';
+    const letters = 'abcdefghijklmnoprstuvzxwq';
+    const symbols = '!~@#$%^&*()-=+-*/.,';
+
+    let validChars = '';
+    if (this.useLetters) {
+      validChars += letters;
+    }
+    if (this.useNumbers) {
+      validChars += numbers;
+    }
+    if (this.useSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPW = '';
+    for (let i = 0; i < this.passwordLength; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPW += validChars[index];
+    }
+    this.password = generatedPW;
+    console.log(this.passwordLength);
   }
   OnChangeUseLetters() {
     this.useLetters = !this.useLetters;
@@ -25,5 +50,12 @@ export class AppComponent {
   }
   OnChangeUseSymbols() {
     this.useSymbols = !this.useSymbols;
+  }
+  OnChangeLength(value: Event) {
+    const target = value.target as HTMLInputElement;
+    // const parsedValue = parseInt(value);
+    !isNaN(+target.value) && (this.passwordLength = +target.value);
+    // if (!isNaN(parsedValue)) {
+    // this.passwordLength = parsedValue;
   }
 }

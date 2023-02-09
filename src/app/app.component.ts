61d2515ff0ab19d147290f3f50c08a4d0f127a13
Arminas Strolys <arminas.strolys@gmail.com>
2023-02-09 20:13:19 +0200

    Figuring out the basics on Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pw-generator';
+  password = '';
+  useLetters = false;
+  useNumbers = false;
+  useSymbols = false;
+  // title = 'pw-generator';
+  onButtonClick() {
+    console.log('l', this.useLetters);
+    console.log('n', this.useNumbers);
+    console.log('s', this.useSymbols);
+    this.password = 'PW';
+  }
+  OnChangeUseLetters() {
+    this.useLetters = !this.useLetters;
+  }
+  OnChangeUseNumbers() {
+    this.useNumbers = !this.useNumbers;
+  }
+  OnChangeUseSymbols() {
+    this.useSymbols = !this.useSymbols;
+  }
 }
